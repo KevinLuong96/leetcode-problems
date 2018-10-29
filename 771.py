e0029abdc30a71aa4f9f5cc3d4649fbde0986b4a
kevinluong96 <k5luong@uwaterloo.ca>
2018-10-28 23:10:10 -0700

    did some more problems

---
+++ 771.py
@@ -0,0 +1,27 @@
+"""
+You're given strings J representing the types of stones that are jewels, 
+and S representing the stones you have.  Each character in S is a type of 
+stone you have.  You want to know how many of the stones you have are also jewels.
+
+The letters in J are guaranteed distinct, and all characters in J and S are letters. 
+Letters are case sensitive, so "a" is considered a different type of stone from "A".
+"""
+
+class Solution(object):
+    def numJewelsInStones(self, J, S):
+        """
+        :type J: str
+        :type S: str
+        :rtype: int
+        """
+        jewels = set()
+        for char in J:
+            jewels.add(char)
+            
+        counter = 0
+        for i in S:
+            if i in jewels:
+                counter += 1
+                
+        return counter
+            
---
+++ 796.py
@@ -0,0 +1,19 @@
+"""
+We are given two strings, A and B.
+
+A shift on A consists of taking string A and moving the leftmost character to the rightmost position. For example, if A = 'abcde', then it will be 'bcdea' after one shift on A. Return True if and only if A can become B after some number of shifts on A.
+"""
+
+class Solution(object):
+    def rotateString(self, A, B):
+        """
+        :type A: str
+        :type B: str
+        :rtype: bool
+        """
+        if len(B)<len(A):
+            return False 
+        
+        A = A+A
+        return True if B in A else False
+        
